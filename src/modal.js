54d5b4532b3331689c0e1cdbb733f9c6bc7b23fd
fourside <fourside@gmail.com>
2018-08-26 21:25:50 +0900

    use localStorage

--- src/modal.js
+++ src/modal.js
@@ -3,17 +3,44 @@
   document.addEventListener("DOMContentLoaded", init);
 
   var modal;
+  var storage;
+  var storageKey = "neverAskMeAgain";
+
   function init() {
+    modal = document.getElementById("modal");
+    storage = localStorage;
+
     var modalOpenBtn = document.getElementById("form-button");
     modalOpenBtn.onclick = openModal;
-    modal = document.getElementById("modal");
+
+    var modalBtn = document.getElementById("modal-button");
+    modalBtn.onclick = clickBtnModal;
+
     var closeBtn = document.getElementById("modal-close");
     closeBtn.onclick = closeModal;
   }
 
   function openModal(event) {
     event.preventDefault();
-    modal.style.display = "block";
+    var neverAskMe = storage.getItem(storageKey);
+    if (neverAskMe) {
+      mainAction();
+    } else {
+      modal.style.display = "block";
+    }
+  }
+
+  function clickBtnModal() {
+    var checkbox = document.getElementById("not-ask-me-again");
+    if (checkbox.checked) {
+      storage.setItem(storageKey, "true");
+    }
+    mainAction();
+    closeModal();
+  }
+
+  function mainAction() {
+    console.log("do main action");
   }
 
   function closeModal() {
